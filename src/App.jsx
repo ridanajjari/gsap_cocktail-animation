226bcfaa7c505d3d963771a906a24959040d3490
ridanajjari <najjaririda6@gmail.com>
2025-07-18 00:10:45 +0100

    implement the new hero animation with gsap

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { ScrollTrigger,SplitText } from 'gsap/all';
 import Navbar from './components/Navbar';
+import Hero from './components/hero';
 import  gsap from 'gsap'
 import React from 'react'
  gsap.registerPlugin(ScrollTrigger,SplitText);
 const App = () => {
   return ( <main>
     <Navbar/> 
+    <Hero/>
+    <div className="h-dvh bg-black">
+
+    </div>
   </main>
   )
   
